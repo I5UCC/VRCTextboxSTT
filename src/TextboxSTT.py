dba6923583e61cfaf11cb1b15546eb362c950557
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-01-03 02:32:45 +0100

    Added Tkinter UI | Additional sound feedback

--- src/TextboxSTT.py
+++ src/TextboxSTT.py
@@ -4,28 +4,47 @@ import traceback
 import time
 import json
 import keyboard
-import warnings
-import speech_recognition as sr
+import numpy as np
+import winsound
+import openvr
 import whisper
 import torch
-import numpy as np
-import ctypes
+import speech_recognition as sr
+import tkinter as tk
 from pythonosc import udp_client
-import openvr
-import winsound
 from colorama import Fore
 
-warnings.filterwarnings("ignore", category=UserWarning)
+ui = tk.Tk()
+ui.minsize(810, 310)
+ui.maxsize(810, 310)
+ui.resizable(False, False)
+ui.configure(bg="#333333")
+ui.title("TextboxSTT")
+
+status_lbl = tk.Label(ui, text="Status")
+status_lbl.configure(bg="#333333", fg="white", font=("Cascadia Code", 12))
+status_lbl.place(relx=0.045, rely=0.07, anchor="w")
+
+color_lbl = tk.Label(ui, text="")
+color_lbl.configure(bg="blue", width=2, fg="white", font=("Cascadia Code", 12))
+color_lbl.place(relx=0.01, rely=0.07, anchor="w")
+
+text_lbl = tk.Label(ui, wraplength=800, text="---")
+text_lbl.configure(bg="#333333", fg="white", font=("Cascadia Code", 27))
+text_lbl.place(relx=0.45, rely=0.55, anchor="center")
+
+def set_status_label(text, color):
+    status_lbl.configure(text=text)
+    color_lbl.configure(bg=color)
+
+def set_text_label(text):
+    text = text[:144]
+    text_lbl.configure(text=text)
 
 VRC_INPUT_PARAM = "/chatbox/input"
 VRC_TYPING_PARAM = "/chatbox/typing"
 ACTIONSETHANDLE = "/actions/textboxstt"
 STTLISTENHANDLE = "/actions/textboxstt/in/sttlisten"
-
-# Set window name on Windows
-if os.name == 'nt':
-    ctypes.windll.kernel32.SetConsoleTitleW("TextboxSTT")
-
 
 def cls():
     """Clears Console"""
@@ -38,14 +57,11 @@ def get_absolute_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-def play_ping():
-    """Plays a ping sound."""
-    winsound.PlaySound('ping.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
+def play_sound(filename):
+    """Plays a wave file."""
+    filename = f"resources/{filename}.wav"
+    winsound.PlaySound(get_absolute_path(filename), winsound.SND_FILENAME | winsound.SND_ASYNC)
 
-
-def play_ping2():
-    """Plays another ping sound."""
-    winsound.PlaySound('ping2.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
 
 config = json.load(open(get_absolute_path('config.json')))
 
@@ -82,23 +98,25 @@ except Exception:
 def listen_and_transcribe():
     with sr.Microphone(sample_rate=16000) as source:
         print(Fore.LIGHTCYAN_EX + "LISTENING")
-        play_ping()
+        set_status_label("LISTENING", "#FF00FF")
+        play_sound("listen")
         try:
-            audio = r.listen(source, timeout=3)
+            audio = r.listen(source, timeout=float(config["timeout_time"]))
         except sr.WaitTimeoutError:
             clear_chatbox()
-            play_ping2()
-            return ""
-        play_ping2()
+            play_sound("timeout")
+            return None
+        play_sound("donelisten")
         torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
         
         oscClient.send_message(VRC_TYPING_PARAM, True)
         print(Fore.LIGHTCYAN_EX + "TRANSCRIBING")
+        set_status_label("TRANSCRIBING", "orange")
         if lang:
             result = audio_model.transcribe(torch_audio, language=lang)
         else:
             result = audio_model.transcribe(torch_audio)
-
+        play_sound("finished")
         return result["text"]
 
 
@@ -107,17 +125,23 @@ def send_message():
     trans = listen_and_transcribe()
     if trans:
         print(Fore.YELLOW + "-" + trans)
+        set_text_label(trans)
         print(Fore.LIGHTCYAN_EX + "POPULATING TEXTBOX")
+        set_status_label("TRANSCRIBING", "#ff8800")
         oscClient.send_message(VRC_INPUT_PARAM, [trans, True, True])
         oscClient.send_message(VRC_TYPING_PARAM, False)
         print(Fore.LIGHTBLUE_EX + "WAITING")
+        set_status_label("WAITING", "#00008b")
 
 
 def clear_chatbox():
     print(Fore.LIGHTCYAN_EX + "CLEARING OSC TEXTBOX")
+    set_status_label("CLEARING OSC TEXTBOX", "#e0ffff")
     oscClient.send_message(VRC_INPUT_PARAM, ["", True])
     oscClient.send_message(VRC_TYPING_PARAM, False)
     print(Fore.LIGHTBLUE_EX + "WAITING")
+    set_status_label("WAITING", "#00008b")
+    set_text_label("---")
 
 
 def get_action_bstate():
@@ -132,10 +156,6 @@ def get_action_bstate():
     return bool(openvr.VRInput().getDigitalActionData(buttonactionhandle, openvr.k_ulInvalidInputValueHandle).bState)
 
 
-def on_hotkey():
-    send_message()
-
-
 def handle_input():
     global held
     # Set up OpenVR events and Action sets
@@ -145,40 +165,40 @@ def handle_input():
 
     if pressed and not held:
         while pressed:
-            if time.time() - curr_time > 1.5:
+            if time.time() - curr_time > float(config["hold_time"]):
                 clear_chatbox()
                 held = True
                 break
             pressed = get_action_bstate()
+            ui.update()
             time.sleep(0.05)
         if not held:
             send_message()
     elif held and not pressed:
         held = False
 
+    ui.after(50, handle_input)
+
 
 held = False
-keyboard.add_hotkey(config["record_hotkey"], on_hotkey)
+keyboard.add_hotkey(config["record_hotkey"], send_message)
 keyboard.add_hotkey(config["clear_hotkey"], clear_chatbox)
 cls()
 print(Fore.GREEN + "-INITIALZIED-")
+set_status_label("INITIALZIED", "green")
 print(Fore.LIGHTBLUE_EX + "WAITING")
+set_status_label("WAITING", "#00008b")
 if ovr_initialized:
-    # Main Loop
-    while True:
-        try:
-            handle_input()
-            time.sleep(0.05)
-        except KeyboardInterrupt:
-            cls()
-            sys.exit()
-        except Exception:
-            cls()
-            print("UNEXPECTED ERROR\n")
-            print("Please Create an Issue on GitHub with the following information:\n")
-            traceback.print_exc()
-            input("\nPress ENTER to exit")
-            sys.exit()
+    try:
+        ui.after(50, handle_input)
+    except Exception:
+        cls()
+        print("UNEXPECTED ERROR\n")
+        print("Please Create an Issue on GitHub with the following information:\n")
+        traceback.print_exc()
+        input("\nPress ENTER to exit")
+        sys.exit()
 else:
     print(Fore.MAGENTA + "OpenVR couldnt be initialized, continuing PC only mode.")
-    keyboard.wait()
+
+ui.mainloop()
--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,7 @@
-import sys
 from cx_Freeze import setup, Executable
 
 packages = ["torch"]
-file_include = ["config.json", "bindings/", "app.vrmanifest", "ping.wav", "ping2.wav"]
+file_include = ["config.json", "bindings/", "resources/", "app.vrmanifest"]
 
 build_exe_options = {"packages": packages, "include_files": file_include}
 setup(
@@ -10,5 +9,5 @@ setup(
     version="0.1",
     description="TextboxSTT",
     options={"build_exe": build_exe_options},
-    executables=[Executable("TextboxSTT.py", targetName="TextboxSTT.exe", base=False), Executable("TextboxSTT.py", targetName="TextboxSTT_NoConsole.exe", base="Win32GUI")],
+    executables=[Executable("TextboxSTT.py", targetName="TextboxSTT.exe", base="Win32GUI"), Executable("TextboxSTT.py", targetName="TextboxSTT_Console.exe", base=False)],
 )
