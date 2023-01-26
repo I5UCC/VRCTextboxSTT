e00b34007258eb46bc31003b9a3aa3bbc83c93f4
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-01-26 13:04:25 +0100

    auto update sync

--- src/TextboxSTT.py
+++ src/TextboxSTT.py
@@ -126,7 +126,7 @@ def init():
     use_both = bool(CONFIG["use_both"])
     if use_kat:
         _kat_sync = False if CONFIG["kat_sync"] else True
-        kat = KatOsc(osc_client, CONFIG["osc_ip"], CONFIG["osc_server_port"], True, None if _kat_sync else int(CONFIG["kat_sync"]))
+        kat = KatOsc(osc_client, CONFIG["osc_ip"], CONFIG["osc_server_port"], _kat_sync, None if _kat_sync else int(CONFIG["kat_sync"]))
     else:
         kat = None
 
