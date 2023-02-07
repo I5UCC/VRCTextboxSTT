3136494944c20411c09aef453bb96141e19cb882
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-02-08 00:22:37 +0100

    revert change devices on runtime

--- src/TextboxSTT.py
+++ src/TextboxSTT.py
@@ -78,8 +78,7 @@ import torch
 import re
 from katosc import KatOsc
 from customthread import CustomThread
-from ui import MainWindow, SettingsWindow, EmoteWindow
-from numba import cuda
+from ui import MainWindow, SettingsWindow
 
 osc_client = None
 kat = None
@@ -144,7 +143,6 @@ def init():
 
     main_window.set_status_label(f"LOADING \"{_whisper_model}\" MODEL", "orange")
     # Load Whisper model
-    print(bool(CONFIG["use_cpu"]) or not torch.cuda.is_available())
     device = "cpu" if bool(CONFIG["use_cpu"]) or not torch.cuda.is_available() else "cuda"
     model = whisper.load_model(_whisper_model, download_root=get_absolute_path("whisper_cache/"), in_memory=True, device=device)
     sys.stderr = StreamToLogger(log, logging.ERROR, LOGFILE)
@@ -282,7 +280,6 @@ def replace_emotes(text):
     for i in range(len(CONFIG["emotes"])):
         word = CONFIG["emotes"][str(i)]
         tmp = re.compile(word, re.IGNORECASE)
-        text = re.sub(f'{word}.', '', text)
         text = tmp.sub(kat.emote_keys[i], text)
 
     return text
@@ -458,27 +455,11 @@ def settings_closing(save=False):
     global kat
     global config_ui
     global config_ui_open
-    global use_cpu
-    global model
-    global osc_client
-    global rec
 
     if save:
         if use_kat:
             kat.stop()
         config_ui.save()
-        try:
-            if not use_cpu and torch.cuda.is_available():
-                print("Clearing CUDA cache...")
-                cuda.select_device(model.device.index)
-                cuda.close()
-            del osc_client
-            del kat
-            del model
-            del use_cpu
-            del rec
-        except Exception as e:
-            pass
         try:
             init()
         except Exception as e:
