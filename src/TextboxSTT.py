88aeccf8a8269e9424b3401d2cac44d55d4158bd
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-02-27 19:37:17 +0100

    bump version number

--- src/TextboxSTT.py
+++ src/TextboxSTT.py
@@ -5,7 +5,7 @@ import logging
 from helper import LogToFile, loadfont, get_absolute_path, play_sound
 
 
-VERSION = "v0.7"
+VERSION = "v0.8"
 ACTIONSETHANDLE = "/actions/textboxstt"
 STTLISTENHANDLE = "/actions/textboxstt/in/sttlisten"
 LOGFILE = get_absolute_path('out.log', __file__)
