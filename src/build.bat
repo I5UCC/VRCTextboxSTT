7f7a065d97d91de0547c101cbc8b29a27cabc005
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-03-20 16:09:04 +0100

    Revert "Revert "Update build.bat""
    This reverts commit c4490a75bd1b0d97d7257e324209948c33f53b4d.

--- src/build.bat
+++ src/build.bat
@@ -4,9 +4,13 @@ set /p CPU=Build CPU Version [Y/n]:
 
 if /I "%CPU%" NEQ "N" set /p CPU7z=Create CPU 7z file? [Y/n]: 
 
+if /I "%CPU%" NEQ "N" set /p CPUDel=Delete Folder after build? [Y/n]: 
+
 set /p GPU=Build GPU Version [Y/n]: 
 
 if /I "%GPU%" NEQ "N" set /p GPU7z=Create GPU 7z file? [Y/n]: 
+
+if /I "%GPU%" NEQ "N" set /p GPUDel=Delete Folder after build? [Y/n]: 
 
 set /p version=< VERSION
 echo %version%
@@ -18,11 +22,16 @@ if /I "%CPU%" NEQ "N" (
 	..\.venv_cpu\Scripts\python.exe -m pip install cx_freeze
 	..\.venv_cpu\Scripts\python.exe .\setup.py build
 	robocopy ..\.venv_cpu\Lib\site-packages\av.libs build\exe.win-amd64-3.10\lib\av.libs /E /NFL /NDL /NJH /NJS /nc /ns /np
-	robocopy build\exe.win-amd64-3.10 build\CPU\TextboxSTT /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
+	robocopy build\exe.win-amd64-3.10 build\TextboxSTT /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 	if /I "%CPU7z%" NEQ "N" (
-		7z a TextboxSTT_%version%_CPU.7z build\CPU\TextboxSTT -mx9
+		if /I "%CPUDel%" NEQ "N" (
+			7z a TextboxSTT_%version%_CPU.7z build\TextboxSTT -mx9 -sdel
+		) else (
+			7z a TextboxSTT_%version%_CPU.7z build\TextboxSTT -mx9
+		)
 		move TextboxSTT_%version%_CPU.7z build
 	)
+	robocopy build\TextboxSTT build\TextboxSTT_CPU /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 )
 
 :: BUILD GPU
@@ -30,11 +39,16 @@ if /I "%GPU%" NEQ "N" (
 	..\.venv_gpu\Scripts\python.exe -m pip install cx_freeze
 	..\.venv_gpu\Scripts\python.exe .\setup.py build
 	robocopy ..\.venv_gpu\Lib\site-packages\av.libs build\exe.win-amd64-3.10\lib\av.libs /E /NFL /NDL /NJH /NJS /nc /ns /np
-	robocopy build\exe.win-amd64-3.10 build\GPU\TextboxSTT /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
+	robocopy build\exe.win-amd64-3.10 build\TextboxSTT /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 	if /I "%GPU7z%" NEQ "N" (
-		7z a TextboxSTT_%version%_GPU.7z build\GPU\TextboxSTT -mx9
+		if /I "%GPUDel%" NEQ "N" (
+			7z a TextboxSTT_%version%_GPU.7z build\TextboxSTT -mx9 -sdel
+		) else (
+			7z a TextboxSTT_%version%_GPU.7z build\TextboxSTT -mx9
+		)
 		move TextboxSTT_%version%_GPU.7z build
 	)
+	robocopy build\TextboxSTT build\TextboxSTT_GPU /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
 )
 
 echo Done building TextboxSTT %version%
