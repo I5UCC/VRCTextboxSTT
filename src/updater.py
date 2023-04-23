395673e31dca29b7a80f7ca5c39ada55d002ca27
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-04-23 17:15:29 +0200

    updater exception handling

--- src/updater.py
+++ src/updater.py
@@ -5,6 +5,7 @@ from helper import get_absolute_path, log
 from subprocess import Popen, PIPE, STDOUT
 import threading
 import os
+import traceback
 
 class Update_Handler(object):
     def __init__(self, repo_path: str = "..", script_path: str = __file__):
@@ -13,27 +14,41 @@ class Update_Handler(object):
         self.repo = git.Repo(repo_path)
 
     def get_latest_tag(self):
-        self.fetch_tags()
-        tags = sorted(self.repo.tags, key=lambda t: t.commit.committed_datetime)
-        latest_tag = tags[-1]
-        return str(latest_tag)
+        try:
+            self.fetch_tags()
+            tags = sorted(self.repo.tags, key=lambda t: t.commit.committed_datetime)
+            latest_tag = tags[-1]
+            return str(latest_tag)
+        except Exception:
+            log.debug(traceback.format_exc())
+            return None
     
     def check_for_updates(self, current_version: str = None) -> tuple:
         if current_version is None:
             return False, None
+
         latest_tag = self.get_latest_tag()
+
+        if latest_tag is None:
+            return False, None
 
         update_available = current_version != latest_tag
 
         return update_available, latest_tag
     
     def fetch_tags(self):
-        res = git.Git(self.repo_path).execute("git fetch --all --tags")
-        log.debug(res)
-    
+        try:
+            res = git.Git(self.repo_path).execute("git fetch --all --tags")
+            log.debug(res)
+        except Exception:
+            log.debug(traceback.format_exc())
+
     def pull(self):
-        res = git.Git(self.repo_path).execute("git pull --rebase --autostash")
-        log.debug(res)
+        try:
+            res = git.Git(self.repo_path).execute("git pull --rebase --autostash")
+            log.debug(res)
+        except Exception:
+            log.debug(traceback.format_exc())
 
     def update(self, callback):
         
@@ -43,7 +58,7 @@ class Update_Handler(object):
             if os.path.isfile(get_absolute_path("../python/CPU", self.script_path)):
                 requirements_file = "requirements.cpu.txt"
 
-            process = Popen([sys.executable, "-m", "pip", "install", "-U", "-r", get_absolute_path(requirements_file, self.script_path)], stdout=PIPE, stderr=STDOUT)
+            process = Popen([sys.executable, "-m", "pip", "install", "-U", "-r", get_absolute_path(requirements_file, self.script_path), "--no-warn-script-location"], stdout=PIPE, stderr=STDOUT)
             with process.stdout:
                 for line in iter(process.stdout.readline, b''):
                     log.debug(str(line)[2:-5])
