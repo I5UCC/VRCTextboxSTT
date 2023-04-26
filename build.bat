8422ee859a2c804b78421fcf37cd914392da4981
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-04-26 18:30:12 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,17 +2,19 @@
 
 cd %~dp0
 
-RMDIR /S /Q VRCTextboxSTT
-RMDIR /S /Q build
+rmdir /S /Q build
+mkdir build
+cd build
 
 git clone -n --depth=1 --filter=tree:0 https://github.com/I5UCC/VRCTextboxSTT
 cd VRCTextboxSTT
 git sparse-checkout set --no-cone src TextboxSTT.exe obs_only.exe
 git checkout
 git fetch --all --tags
+
 cd ..
-robocopy VRCTextboxSTT build/TextboxSTT /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
-7z x python.zip -obuild/TextboxSTT
-set /p version=< src/VERSION
+robocopy VRCTextboxSTT TextboxSTT /MOVE /E /NFL /NDL /NJH /NJS /nc /ns /np
+7z x ../python.zip -oTextboxSTT
+set /p version=< ../src/VERSION
 cd build
 7z a TextboxSTT_%version%.zip TextboxSTT
