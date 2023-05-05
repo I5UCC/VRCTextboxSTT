0962593456d157ae59a515ec6dc29932faadf414
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-05-05 14:12:02 +0200

    run hidden attempt

--- src/updater.py
+++ src/updater.py
@@ -1,6 +1,6 @@
 import sys
 from helper import get_absolute_path, log
-from subprocess import run, Popen, PIPE, STDOUT
+from subprocess import run, Popen, PIPE, STDOUT, STARTUPINFO, STARTF_USESHOWWINDOW
 import threading
 import os
 import traceback
@@ -13,11 +13,14 @@ class Update_Handler(object):
         if not os.path.isfile(self.git_path):
             log.error("Git not found, using default path")
             self.git_path = "git"
+        self.startupinfo = STARTUPINFO()
+        self.startupinfo.dwFlags = STARTF_USESHOWWINDOW
+        self.startupinfo.wShowWindow = 0
 
     def get_latest_tag(self):
         try:
             self.fetch()
-            result = run([self.git_path, "tag"], cwd=self.repo_path, stdout=PIPE)
+            result = run([self.git_path, "tag"], cwd=self.repo_path, stdout=PIPE, startupinfo=self.startupinfo)
             tags = result.stdout.decode('utf-8')
             latest_tag = tags[tags.rfind("v"):].strip()
             return latest_tag
@@ -42,7 +45,7 @@ class Update_Handler(object):
     def fetch(self):
         try:
             log.debug("Fetching Tags")
-            result = run([self.git_path, "fetch", "--all", "--tags"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT)
+            result = run([self.git_path, "fetch", "--all", "--tags"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT, startupinfo=self.startupinfo)
             log.debug(result.stdout.decode('utf-8'))
         except Exception:
             log.debug(traceback.format_exc())
@@ -50,9 +53,9 @@ class Update_Handler(object):
     def pull(self):
         try:
             self.fetch()
-            result = run([self.git_path, "reset", "--hard"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT)
+            result = run([self.git_path, "reset", "--hard"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT, startupinfo=self.startupinfo)
             log.debug(result.stdout.decode('utf-8'))
-            result = run([self.git_path, "pull"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT)
+            result = run([self.git_path, "pull"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT, startupinfo=self.startupinfo)
             log.debug(result.stdout.decode('utf-8'))
         except Exception:
             log.debug(traceback.format_exc())
@@ -65,12 +68,12 @@ class Update_Handler(object):
             if os.path.isfile(get_absolute_path("../python/CPU", self.script_path)):
                 requirements_file = "requirements.cpu.txt"
 
-            process = Popen([sys.executable, "-m", "pip", "install", "-U", "-r", get_absolute_path(requirements_file, self.script_path), "--no-warn-script-location"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT)
+            process = Popen([sys.executable, "-m", "pip", "install", "-U", "-r", get_absolute_path(requirements_file, self.script_path), "--no-warn-script-location"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT, startupinfo=self.startupinfo)
             with process.stdout:
                 for line in iter(process.stdout.readline, b''):
                     log.debug(str(line)[2:-5])
             process.wait()
-            process = Popen([sys.executable, "-m", "pip", "cache", "purge"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT)
+            process = Popen([sys.executable, "-m", "pip", "cache", "purge"], cwd=self.repo_path, stdout=PIPE, stderr=STDOUT, startupinfo=self.startupinfo)
             with process.stdout:
                 for line in iter(process.stdout.readline, b''):
                     log.debug(str(line)[2:-5])
