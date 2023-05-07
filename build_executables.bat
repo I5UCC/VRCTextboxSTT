d161a200148b9410036be345e2c2404e6e473b2c
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-05-08 00:58:33 +0200

    x64 executable

--- build_executables.bat
+++ build_executables.bat
@@ -4,7 +4,7 @@ cd %~dp0
 del /f .\TextboxSTT.exe
 del /f .\obs_only.exe
 
-Bat_To_Exe_Converter.exe /bat .\TextboxSTT.bat /exe .\TextboxSTT.exe /icon .\src\resources\icon.ico
-Bat_To_Exe_Converter.exe /bat .\obs_only.bat /exe .\obs_only.exe /icon .\src\resources\icon.ico
+Bat_To_Exe_Converter.exe /bat .\TextboxSTT.bat /exe .\TextboxSTT.exe /icon .\src\resources\icon.ico /x64 /copyright I5UCC
+Bat_To_Exe_Converter.exe /bat .\obs_only.bat /exe .\obs_only.exe /icon .\src\resources\icon.ico /x64 /copyright I5UCC
 
 timeout 10
