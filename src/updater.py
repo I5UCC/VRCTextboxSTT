17597546ba20bef1aacbbfdc1483fab1a52b7168
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-08-09 22:10:29 +0200

    updater ignore version words

--- src/updater.py
+++ src/updater.py
@@ -6,6 +6,7 @@ import os
 import traceback
 import logging
 import shutil
+import re
 
 log = logging.getLogger(__name__)
 
@@ -44,6 +45,8 @@ class Update_Handler(object):
         if latest_tag is None:
             return False, None
 
+        current_version = re.sub(r'-.+', "", current_version)
+        latest_tag = re.sub(r'-.+', "", latest_tag)
         update_available = current_version != latest_tag
 
         return update_available, latest_tag
