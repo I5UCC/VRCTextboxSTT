d49495fd0914bd979766f60610d4aea5c08c8e17
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-11-11 20:27:29 +0100

    add distil whisper models

--- src/config.py
+++ src/config.py
@@ -118,6 +118,8 @@ WHISPER_MODELS = {
     'medium.en': 'openai/whisper-medium.en',
     'large': 'openai/whisper-large',
     'large-v2': 'openai/whisper-large-v2',
+    'distil-medium': "distil-whisper/distil-medium.en",
+    'distil-large-v2': "distil-whisper/distil-large-v2"
 }
 
 TRANSLATE_MODELS = {
