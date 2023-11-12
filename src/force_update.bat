0ba113dac4f08a9d1ec5081b5112ae476f10d1ef
Isaak <43730681+I5UCC@users.noreply.github.com>
2023-11-12 18:36:52 +0100

    run force_update to update

--- src/force_update.bat
+++ src/force_update.bat
@@ -1,23 +1,20 @@
 @echo off
 
-cd %~dp0\..\python\Lib\site-packages
-@ECHO OFF
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S "*~*"') DO (
-    RMDIR /S /Q "%%G"
-)
+TITLE TextboxSTT Updater
 
 cd %~dp0\..
 .\git\bin\git.exe reset --hard
 .\git\bin\git.exe pull --rebase
 if exist ../python/CPU (
 	echo reinstalling CPU dependencies
-	.\python\python.exe -m pip install -U -r .\src\requirements.cpu.txt
+	.\python\python.exe -m pip install -U -r .\src\requirements.cpu.txt --no-warn-script-location
 ) else (
 	echo reinstalling all dependencies
-	.\python\python.exe -m pip install -U -r .\src\requirements.txt
+	.\python\python.exe -m pip install -U -r .\src\requirements.txt --no-warn-script-location
 )
 
 echo.
 echo.
 echo Done! Press any key to exit...
 pause >nul
+exit /b %errorlevel%
