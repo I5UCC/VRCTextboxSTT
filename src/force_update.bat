f26eac46346a150161475d6bfd2befcc620e510b
Isaak <I5UCC@pm.me>
2023-11-27 23:11:29 +0100

    updater check for changes

--- src/force_update.bat
+++ src/force_update.bat
@@ -3,8 +3,24 @@
 TITLE TextboxSTT Updater
 
 cd /d %~dp0\..
-.\git\bin\git.exe reset --hard
-.\git\bin\git.exe pull --rebase
+
+.\git\bin\git.exe status --porcelain --untracked-files=no | find /i "M"
+
+if not errorlevel 1  (
+	set /p "answer=You've made changes to some base files, do you want to stash your changes [Y/n] "
+) else (
+	set answer=n
+)
+
+if /i "%answer%" EQU "n" (
+	.\git\bin\git.exe reset --hard
+	.\git\bin\git.exe pull --rebase
+) else (
+	.\git\bin\git.exe stash
+	.\git\bin\git.exe pull --rebase
+	.\git\bin\git.exe stash pop
+)
+
 .\python\python.exe -m pip install --upgrade pip
 if exist python/CPU (
 	echo reinstalling CPU dependencies
