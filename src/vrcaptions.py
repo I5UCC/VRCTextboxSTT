37c8beae08ff794bba17170513ca99f609665611
Isaak <I5UCC@pm.me>
2024-03-23 00:40:27 +0100

    rewrite the obs_only script to instead output to ovr only

--- src/vrcaptions.py
+++ src/vrcaptions.py
@@ -5,23 +5,62 @@ DEBUG = len(sys.argv) == 1
 from time import sleep, time
 from listen import ListenHandler
 from transcribe import TranscribeHandler
-from translate import TranslationHandler
 from ovr import OVRHandler
+from translate import TranslationHandler
 from helper import get_absolute_path, replace_words
 import keyboard
 from config import config_struct
 import numpy as np
+import re
+import traceback
+from ui import SettingsWindow
+import subprocess
+from logger import force_single_instance
+
+def restart(save: bool = False) -> None:
+    """
+    Restarts the program.
+    """
+    global settings_window
+    if save:
+        settings_window.save()
+    
+    settings_window.on_closing()
+    subprocess.Popen([sys.executable, *sys.argv])
+    sys.exit(0)
+
+def open_settings(config, config_path) -> None:
+    global settings_window
+
+    def get_coordinates():
+        return (50, 50)
+
+    settings_window = SettingsWindow(config, config_path, __file__, get_coordinates, get_coordinates)
+    settings_window.tkui.protocol("WM_DELETE_WINDOW", restart)
+    settings_window.btn_save.config(command=lambda: restart(True))
+    settings_window.open()
 
 def main():
-    global enabled, listen
-    
-    if DEBUG:
-        CACHE_PATH = get_absolute_path('cache/', __file__)
-        CONFIG_PATH = get_absolute_path('config.json', __file__)
-    else:
-        CACHE_PATH = get_absolute_path('../cache/', __file__)
-        CONFIG_PATH = get_absolute_path('../config.json', __file__)
+    global enabled, listen, settings_window
+
+    CACHE_PATH = get_absolute_path('../cache/', __file__)
+    CONFIGS_PATH = get_absolute_path('../configs/', __file__)
+
+    try:
+        os.mkdir(CONFIGS_PATH)
+    except FileExistsError:
+        pass
+    except Exception:
+        print("Failed to create cache directory: ")
+        print(traceback.format_exc())
+
+    CONFIG_PATH = os.path.join(CONFIGS_PATH, "obs_only.json")
+    if not os.path.exists(CONFIG_PATH):
+        config_struct.save(config_struct(), CONFIG_PATH)
     config: config_struct = config_struct.load(CONFIG_PATH)
+
+    replacement_dict = {re.compile(key, re.IGNORECASE): value for key, value in config.wordreplacement.list.items()}
+    base_replacement_dict = {re.compile(key, re.IGNORECASE): value for key, value in config.wordreplacement.base_replacements.items()}
 
     if config.listener.pause_threshold < 3.0:
         config.listener.pause_threshold = 3.0
@@ -32,17 +71,23 @@ def main():
 
     listen = ListenHandler(config.listener)
     transcriber = TranscribeHandler(config.whisper, config.vad, CACHE_PATH, config.translator.language == "english")
-    transcriber.transcribe(np.zeros(100000, dtype=np.float32))
+    transcriber.transcribe()
     translator: TranslationHandler = None
     if config.translator.language and config.translator.language != config.whisper.language and transcriber.task == "transcribe":
         translator = TranslationHandler(CACHE_PATH, config.whisper.language, config.translator)
-    ovr: OVRHandler = OVRHandler(config.overlay, __file__, DEBUG)
-    if OVRHandler.is_running():
+    font_language = config.whisper.language if not config.translator.language else config.translator.language
+    ovr = OVRHandler(config.overlay, __file__, font_language, DEBUG)
+    while not ovr.is_running():
         ovr.init()
+        sleep(3)
+    if ovr.overlay_font != font_language:
+        ovr.set_overlay_font(font_language)
 
     phrase_timeout = config.listener.timeout_time
     clear_timeout = config.text_timeout
     max_transciption_time = config.whisper.max_transciption_time
+    max_samples = config.whisper.max_samples
+    cutoff_buffer = config.whisper.cutoff_buffer
     toggle_hotkey = config.hotkey
     energy_threshold = config.listener.energy_threshold
 
@@ -74,7 +119,7 @@ def main():
         print("Adjusting for ambient noise. Please wait a moment and be silent.")
         listen.rec.energy_threshold = listen.get_energy_threshold()
 
-    print("----------------------- VRCaptions -----------------------")
+    print("---------------- OBS Whisper Transcriber -----------------")
     print("Press {} to toggle listening.".format(toggle_hotkey))
     print("Press Ctrl+C to exit, or close the console window.")
     print("----------------------------------------------------------")
@@ -82,16 +127,22 @@ def main():
     print("phrase_timeout:\t\t{}".format(phrase_timeout))
     print("clear_timeout:\t\t{}".format(clear_timeout))
     print("max_transciption_time:\t{}".format(max_transciption_time))
+    print("max_samples:\t\t{}".format(max_samples))
+    print("cutoff_buffer:\t\t{}".format(cutoff_buffer))
     print("energy_threshold:\t{}".format(energy_threshold))
     print("vad:\t\t\t{}".format(config.vad.enabled))
     print()
     print(transcriber.device_name, transcriber.whisper_model, transcriber.compute_type)
+    print()
+    print("Press F12 to open settings.")
 
     listen.start_listen_background()
 
     print("------------------------ LISTENING -----------------------")
     while True:
         try:
+            if keyboard.is_pressed("F12"):
+                open_settings(config, CONFIG_PATH)
             time_last = time()
 
             if not listen.data_queue.empty():
@@ -107,6 +158,9 @@ def main():
 
                     print("----------------------------------------------------------")
                     text = transcriber.transcribe(torch_audio)
+                    if not text:
+                        continue
+
                     if translator:
                         text = translator.translate(text)
                     if append and text:
@@ -116,17 +170,22 @@ def main():
                     print(f"Time taken:\t{time_taken:.5f}s (max: {max_transciption_time}s)")
                     print(f"bytes:\t\t{len(last_sample)}\ntext_length:\t{len(text)}")
                     if config.wordreplacement.enabled:
-                        text = replace_words(text, config.wordreplacement.list)
-                        text = replace_words(text, config.wordreplacement.base_replacements)
+                        text = replace_words(text, replacement_dict)
+                        text = replace_words(text, base_replacement_dict)
+
+                    if not text:
+                        continue
+
                     ovr.set_overlay_text(text)
                     print("- " + text if text else "No text found")
-                    sentence_end = text[-1] in [".", "!", "?"]
-                    first_run = False
 
-                    if time_taken > max_transciption_time and sentence_end and not first_run:
+                    first_run = False
+                    sentence_end = text and text[-1] in {".", "!", "?"}
+                    if sentence_end and not first_run and (len(last_sample) > max_samples or time_taken > max_transciption_time):
+                        print("------------------------ CUTOFF ----------------------")
                         last_text = text + " "
                         append = True
-                        last_sample = bytes()
+                        last_sample = last_sample[-cutoff_buffer:]
                 except Exception as e:
                     print(e)
                 phrase_time = time()
@@ -143,8 +202,11 @@ def main():
             sleep(0.1)
         except KeyboardInterrupt:
             listen.stop_listen_background()
+            ovr.shutdown()
             break
 
 if __name__=='__main__':
+    if not DEBUG:
+        force_single_instance()
     os.system('cls' if os.name == 'nt' else 'clear')
     main()
